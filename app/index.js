81dceeed5db6da625addc8a5e2e8b82fd7de6445
gj <gj@xkeshi.com>
2018-03-23 18:22:48 +0800

    feat: 定制部分文件

--- app/index.js
+++ app/index.js
@@ -1,6 +1,22 @@
 const Generator = require('yeoman-generator');
+let config = {}
 
 module.exports = class extends Generator {
+  prompting() {
+    return this.prompt(
+      [
+        {
+          type    : 'input',
+          name    : 'name',
+          message : 'Your project name',
+          default : this.appname // Default to current folder name
+        },
+      ]).then((answers) => {
+        config = answers
+      }
+    );
+  }
+
   writing() {
     this.fs.copy(
       this.templatePath('webpack-demo'),
@@ -10,11 +26,23 @@ module.exports = class extends Generator {
           ignore: [
             this.templatePath('webpack-demo/.git'),
             this.templatePath('webpack-demo/demo/**'),
+            this.templatePath('webpack-demo/package.json'),
+            this.templatePath('webpack-demo/package-lock.json'),
+            this.templatePath('webpack-demo/README.md'),
           ],
           dot: true
         }
       }
     );
+    this.fs.copyTpl(
+      this.templatePath('init/package.json'),
+      this.destinationPath('package.json'),
+      config
+    )
+    this.fs.copyTpl(
+      this.templatePath('init/README.md'),
+      this.destinationPath('README.md'),
+    )
   }
 
   install() {
